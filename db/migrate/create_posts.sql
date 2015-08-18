466803422b164268f5f045cb5a778829b229e90d
dru.jensen <dru.jensen@reachlocal.com>
2015-08-18 08:28:30 -0700

    use the model to drop and create the database.

--- db/migrate/create_posts.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE posts (id INT NOT NULL AUTO_INCREMENT, 
-                    name VARCHAR(255), 
-                    body TEXT, 
-                    created_at DATE, 
-                    updated_at DATE, 
-                    PRIMARY KEY (id));
