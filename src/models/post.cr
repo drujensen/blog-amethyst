a415ed50301bfbafd5e83611d548ed623f763593
dru.jensen <dru.jensen@reachlocal.com>
2015-08-29 06:39:15 -0700

    rollback casting
    If the field can be Nil, we still need to handle that situation.

--- src/models/post.cr
+++ src/models/post.cr
@@ -8,13 +8,19 @@ class Post < Model
   fields({ name: "VARCHAR(255)", body: "TEXT" })
 
   def last_updated
-    last_updated = updated_at as Time
-    formatter = TimeFormat.new("%B %d, %Y")
-    last_updated = formatter.format(last_updated)
+    last_updated = updated_at
+    if last_updated.is_a?(Time)
+      formatter = TimeFormat.new("%B %d, %Y")
+      last_updated = formatter.format(last_updated)
+    end
+    return last_updated
   end
 
   def markdown_body
-    markdown_body = body as String
-    markdown_body = Markdown.to_html(markdown_body)
+    markdown_body = body
+    if markdown_body.is_a?(String)
+      markdown_body = Markdown.to_html(markdown_body)
+    end
+    return markdown_body
   end
 end
