6ac42e12199d26bed2542a63b5df50e11b589e20
dru.jensen <dru.jensen@reachlocal.com>
2015-09-03 20:22:46 -0700

    support active-record.cr
    Make changes to support waterlink/active-record.cr.  Currently it doesn't
    support DateTime types so I cannot continue until that gets added.

--- src/models/post.cr
+++ src/models/post.cr
@@ -1,11 +1,47 @@
-require "amethyst-model"
 #require "../../amethyst-model/src/amethyst-model"
+require "active_record"
+require "mysql_adapter"
 require "markdown/markdown"
 
-include Amethyst::Model
+class Post < ActiveRecord::Model
+  name Post
+  adapter mysql
 
-class Post < Model
-  fields({ name: "VARCHAR(255)", body: "TEXT" })
+  primary id :: Int  
+  field name :: String
+  field body :: String
+  field created_at :: Time
+  field updated_at :: Time
+
+
+  def self.all criteria
+    Post.index
+  end
+
+  def self.clear
+    Post.index.each do |post|
+      post.delete
+    end
+  end
+
+  def self.find id
+    Post.read id
+  end
+
+  def save
+    if id
+      created_at = Time.now
+      updated_at = Time.now
+      create
+    else
+      updated_at = Time.now
+      update
+    end
+  end
+
+  def destroy
+    delete
+  end
 
   def last_updated
     last_updated = updated_at
